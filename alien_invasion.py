e25a6572ba76af26d46f54fa352261474e0fe1a7
Yongliang Liu <519594562@qq.com>
2021-07-16 15:54:59 +0800

    更新

--- alien_invasion.py
+++ alien_invasion.py
@@ -12,20 +12,22 @@ def run_game():
     # screen = pygame.display.set_mode((1200,800)) 以上的代码的调用和替换
     pygame.display.set_caption("Alien Invasion")
 
-    ship = Ship(screen)
+    ship = Ship(ai_settings,screen)
     # 创建一艘飞船
     pygame.display.update()
     while True:
-        gf.check_events()
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
+        gf.check_events(ship)
+        ship.update()
+        gf.update_screen(ai_settings,screen,ship)
+       # for event in pygame.event.get():
+       #     if event.type == pygame.QUIT:
+       #         sys.exit()
 
-        screen.fill(ai_settings.bg_color)  # 每次循环都绘制屏幕
+       # screen.fill(ai_settings.bg_color)  # 每次循环都绘制屏幕
         # bg_color = (230,230,230) #背景加上颜色  代码的替换和调用
-        ship.blitme()
+       # ship.blitme()
 
         # 让最近绘制的屏幕可见
-        pygame.display.flip()
+       # pygame.display.flip()
 
 run_game()
