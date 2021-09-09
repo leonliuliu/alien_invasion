746d6cebf7e43f90ddf89609b74b0a49bf914c48
Yongliang Liu <519594562@qq.com>
2021-09-09 11:56:21 +0800

    refomat code

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,6 +4,7 @@ from settings import Settings
 from ship import Ship
 import game_functions as gf
 
+
 def run_game():
     pygame.init()
     ai_settings = Settings()
@@ -12,22 +13,23 @@ def run_game():
     # screen = pygame.display.set_mode((1200,800)) 以上的代码的调用和替换
     pygame.display.set_caption("Alien Invasion")
 
-    ship = Ship(ai_settings,screen)
+    ship = Ship(ai_settings, screen)
     # 创建一艘飞船
     pygame.display.update()
     while True:
         gf.check_events(ship)
         ship.update()
-        gf.update_screen(ai_settings,screen,ship)
-       # for event in pygame.event.get():
-       #     if event.type == pygame.QUIT:
-       #         sys.exit()
+        gf.update_screen(ai_settings, screen, ship)
+    # for event in pygame.event.get():
+    #     if event.type == pygame.QUIT:
+    #         sys.exit()
 
-       # screen.fill(ai_settings.bg_color)  # 每次循环都绘制屏幕
-        # bg_color = (230,230,230) #背景加上颜色  代码的替换和调用
-       # ship.blitme()
+    # screen.fill(ai_settings.bg_color)  # 每次循环都绘制屏幕
+    # bg_color = (230,230,230) #背景加上颜色  代码的替换和调用
+    # ship.blitme()
 
-        # 让最近绘制的屏幕可见
-       # pygame.display.flip()
+    # 让最近绘制的屏幕可见
+    # pygame.display.flip()
+
 
 run_game()
--- game_functions.py
+++ game_functions.py
@@ -1,37 +1,41 @@
 import sys
 import pygame
 
+
 def check_keydown_events(event, ship):
-    #响应按键
+    # 响应按键
     if event.key == pygame.K_RIGHT:
         ship.moving_right = True
     elif event.key == pygame.K_LEFT:
         ship.moving_left = True
 
+
 def check_keyup_events(event, ship):
-    #响应松开
+    # 响应松开
     if event.key == pygame.K_RIGHT:
         ship.moving_right = False
     elif event.key == pygame.K_LEFT:
         ship.moving_left = False
 
+
 def check_events(ship):
-    #响应案件和鼠标移动的事件
+    # 响应案件和鼠标移动的事件
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
 
         elif event.type == pygame.KEYDOWN:
-            check_keydown_events(event,ship)
+            check_keydown_events(event, ship)
 
         elif event.type == pygame.KEYUP:
-            check_keyup_events(event,ship)
+            check_keyup_events(event, ship)
 
-def update_screen(ai_settings,screen,ship):
-    #更新屏幕的图像，并切换到新的屏幕中
-    #每次循环都重新绘制屏幕
+
+def update_screen(ai_settings, screen, ship):
+    # 更新屏幕的图像，并切换到新的屏幕中
+    # 每次循环都重新绘制屏幕
     screen.fill(ai_settings.bg_color)
     ship.blitme()
 
-    #让最新的屏幕可见
+    # 让最新的屏幕可见
     pygame.display.flip()
